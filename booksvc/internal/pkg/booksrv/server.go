b43a8f40a2d398ca7fa89180c3420bdbeab5a324
Hoseong Hwang <liam.hwang@buzzvil.com>
2019-03-16 09:53:17 +0900

    Replace traefik with envoy

--- booksvc/internal/pkg/booksrv/server.go
+++ booksvc/internal/pkg/booksrv/server.go
@@ -15,13 +15,27 @@ func NewServer() pb.BookServiceServer {
 	return &server{}
 }
 
-func (s *server) SearchBooks(c context.Context, request *pb.BooksQueryRequest) (*pb.BooksResponse, error) {
-	return &pb.BooksResponse{
+func (s *server) ListBooks(c context.Context, r *pb.ListBooksRequest) (*pb.ListBooksResponse, error) {
+	return &pb.ListBooksResponse{
 		Books: []*pb.Book{
 			&pb.Book{
 				Name: "Harry Potter",
 				Id:   1,
 			},
 		},
+	}, nil
+}
+
+func (s *server) GetBook(c context.Context, r *pb.GetBookRequest) (*pb.Book, error) {
+	return &pb.Book{
+		Name: "Harry Potter",
+		Id:   1,
+	}, nil
+}
+
+func (s *server) CreateBook(c context.Context, r *pb.CreateBookRequest) (*pb.Book, error) {
+	return &pb.Book{
+		Name: "Harry Potter",
+		Id:   1,
 	}, nil
 }
