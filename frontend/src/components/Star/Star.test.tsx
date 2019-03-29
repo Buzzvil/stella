6100dedc50b7937d8fd15dfed072845830fb0f44
Benjamin Baldivia <goblinlordx@gmail.com>
2019-03-29 09:46:47 +0900

    code style

--- frontend/src/components/Star/Star.test.tsx
+++ frontend/src/components/Star/Star.test.tsx
@@ -4,12 +4,13 @@ import renderer from "react-test-renderer";
 import Star from "./Star";
 
 it("displays with empty image when value is 0", () => {
-    expect(renderer.create(<Star value={0}/>)).toMatchSnapshot()
+  expect(renderer.create(<Star value={0} />)).toMatchSnapshot();
 });
 
 it("displays with empty image when value is .5", () => {
-    expect(renderer.create(<Star value={.5}/>)).toMatchSnapshot()
+  expect(renderer.create(<Star value={0.5} />)).toMatchSnapshot();
 });
+
 it("displays with empty image when value is 1", () => {
-    expect(renderer.create(<Star value={1}/>)).toMatchSnapshot()
+  expect(renderer.create(<Star value={0.5} />)).toMatchSnapshot();
 });
