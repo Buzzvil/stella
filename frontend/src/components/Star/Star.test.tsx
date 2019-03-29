32e73c4a4a98594c0ca2f9831668d93c39a85b49
Benjamin Baldivia <goblinlordx@gmail.com>
2019-03-29 09:28:56 +0900

    Add tests for Star componennt

---
+++ frontend/src/components/Star/Star.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import Star from "./Star";
+
+it("displays with empty image when value is 0", () => {
+    expect(renderer.create(<Star value={0}/>)).toMatchSnapshot()
+});
+
+it("displays with empty image when value is .5", () => {
+    expect(renderer.create(<Star value={.5}/>)).toMatchSnapshot()
+});
+it("displays with empty image when value is 1", () => {
+    expect(renderer.create(<Star value={1}/>)).toMatchSnapshot()
+});
