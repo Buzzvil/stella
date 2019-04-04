c6feabb2ad59ef1d7d6b31f097592a89f655b6b7
Benjamin Baldivia <goblinlordx@gmail.com>
2019-04-04 14:14:04 +0900

    style

--- frontend/src/components/Flag/Flag.test.tsx
+++ frontend/src/components/Flag/Flag.test.tsx
@@ -4,7 +4,7 @@ import renderer from "react-test-renderer";
 import Flag from "./Flag";
 
 it("renders single button", () => {
-  const instance = renderer.create(<Flag/>).root;
+  const instance = renderer.create(<Flag />).root;
   const buttons = instance.findAllByType("button");
   expect(buttons.length).toEqual(1);
 });
