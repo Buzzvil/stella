b0c81cf504a6ad7110329ff19c76e3a835a15974
phil.lee <phil.lee@buzzvil.com>
2019-04-11 12:52:57 +0900

    status styling

--- frontend/src/components/Status/Status.stories.tsx
+++ frontend/src/components/Status/Status.stories.tsx
@@ -6,8 +6,14 @@ import { MuiThemeProvider } from "@material-ui/core";
 import StandardTheme from "../../theme/StandardTheme";
 import Status from "./Status";
 
-storiesOf("Status", module).add("Default", () => (
+storiesOf("Status", module)
+.add("On", () => (
     <MuiThemeProvider theme={StandardTheme}>
         <Status free={true} />
+    </MuiThemeProvider>
+))
+.add("Off", () => (
+    <MuiThemeProvider theme={StandardTheme}>
+        <Status free={false} />
     </MuiThemeProvider>
 ));
--- frontend/src/components/Status/Status.tsx
+++ frontend/src/components/Status/Status.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Fab, { FabProps } from "@material-ui/core/Fab";
+import CloseIcon from "@material-ui/icons/Close";
+import CheckIcon from "@material-ui/icons/Check";
 
 interface StatusProps extends FabProps {
   free: boolean;
 }
 
+const style = {
+  boxShadow: "none",
+};
+
 const Status: React.SFC<StatusProps> = ({ free, style: propsStyle = {}, ...props }) => (
-  <Fab variant="extended" color={free ? "primary" : "secondary" } style={{ ...propsStyle}} {...props}>
+  <Fab variant="round" color={free ? "primary" : "secondary" } style={{...style, ...propsStyle}} {...props}>
+    {free ? <CheckIcon /> : <CloseIcon />}
   </Fab>
 );
 
