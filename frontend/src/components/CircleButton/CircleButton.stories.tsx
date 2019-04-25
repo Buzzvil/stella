04bedb7082ad9c79ffcc7408fca6af6b7fba359a
Benjamin Baldivia <goblinlordx@gmail.com>
2019-04-25 12:48:50 +0900

    Import as inlined svg

--- frontend/src/components/CircleButton/CircleButton.stories.tsx
+++ frontend/src/components/CircleButton/CircleButton.stories.tsx
@@ -5,12 +5,12 @@ import { MuiThemeProvider } from "@material-ui/core";
 
 import StandardTheme from "../../theme/StandardTheme";
 import CircleButton from "./CircleButton";
-import FaceProfileIcon from "../../img/face-profile.svg";
+import { ReactComponent as FaceProfileIcon } from "../../img/face-profile.svg";
 
 storiesOf("CircleButton", module).add("Primary", () => (
   <MuiThemeProvider theme={StandardTheme}>
     <CircleButton>
-        <img src={FaceProfileIcon}/>
+       <FaceProfileIcon />
     </CircleButton>
   </MuiThemeProvider>
 ));
