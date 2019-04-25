ae3f44dc4b5e770864df07c7fcf1d1a3f2c0823f
Yongho Kim <howard.kim@buzzvil.com>
2019-04-25 09:16:39 +0900

    modify rating proto

--- ratingsvc/pkg/proto/ratingsvc.pb.go
+++ ratingsvc/pkg/proto/ratingsvc.pb.go
@@ -90,6 +90,7 @@ func (m *Rating) GetComment() string {
 
 type GetRatingRequest struct {
 	EntityId             int64    `protobuf:"varint,1,opt,name=entity_id,json=entityId,proto3" json:"entity_id,omitempty"`
+	UserId               int64    `protobuf:"varint,2,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -127,6 +128,91 @@ func (m *GetRatingRequest) GetEntityId() int64 {
 	return 0
 }
 
+func (m *GetRatingRequest) GetUserId() int64 {
+	if m != nil {
+		return m.UserId
+	}
+	return 0
+}
+
+type ListRatingRequests struct {
+	EntityId             int64    `protobuf:"varint,1,opt,name=entity_id,json=entityId,proto3" json:"entity_id,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *ListRatingRequests) Reset()         { *m = ListRatingRequests{} }
+func (m *ListRatingRequests) String() string { return proto.CompactTextString(m) }
+func (*ListRatingRequests) ProtoMessage()    {}
+func (*ListRatingRequests) Descriptor() ([]byte, []int) {
+	return fileDescriptor_61e79acbe641d6ef, []int{2}
+}
+
+func (m *ListRatingRequests) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_ListRatingRequests.Unmarshal(m, b)
+}
+func (m *ListRatingRequests) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_ListRatingRequests.Marshal(b, m, deterministic)
+}
+func (m *ListRatingRequests) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ListRatingRequests.Merge(m, src)
+}
+func (m *ListRatingRequests) XXX_Size() int {
+	return xxx_messageInfo_ListRatingRequests.Size(m)
+}
+func (m *ListRatingRequests) XXX_DiscardUnknown() {
+	xxx_messageInfo_ListRatingRequests.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ListRatingRequests proto.InternalMessageInfo
+
+func (m *ListRatingRequests) GetEntityId() int64 {
+	if m != nil {
+		return m.EntityId
+	}
+	return 0
+}
+
+type ListRatingsResponse struct {
+	Ratings              []*Rating `protobuf:"bytes,1,rep,name=ratings,proto3" json:"ratings,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}  `json:"-"`
+	XXX_unrecognized     []byte    `json:"-"`
+	XXX_sizecache        int32     `json:"-"`
+}
+
+func (m *ListRatingsResponse) Reset()         { *m = ListRatingsResponse{} }
+func (m *ListRatingsResponse) String() string { return proto.CompactTextString(m) }
+func (*ListRatingsResponse) ProtoMessage()    {}
+func (*ListRatingsResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_61e79acbe641d6ef, []int{3}
+}
+
+func (m *ListRatingsResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_ListRatingsResponse.Unmarshal(m, b)
+}
+func (m *ListRatingsResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_ListRatingsResponse.Marshal(b, m, deterministic)
+}
+func (m *ListRatingsResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ListRatingsResponse.Merge(m, src)
+}
+func (m *ListRatingsResponse) XXX_Size() int {
+	return xxx_messageInfo_ListRatingsResponse.Size(m)
+}
+func (m *ListRatingsResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_ListRatingsResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ListRatingsResponse proto.InternalMessageInfo
+
+func (m *ListRatingsResponse) GetRatings() []*Rating {
+	if m != nil {
+		return m.Ratings
+	}
+	return nil
+}
+
 type UpsertRatingRequest struct {
 	EntityId             int64    `protobuf:"varint,1,opt,name=entity_id,json=entityId,proto3" json:"entity_id,omitempty"`
 	UserId               int64    `protobuf:"varint,2,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
@@ -141,7 +227,7 @@ func (m *UpsertRatingRequest) Reset()         { *m = UpsertRatingRequest{} }
 func (m *UpsertRatingRequest) String() string { return proto.CompactTextString(m) }
 func (*UpsertRatingRequest) ProtoMessage()    {}
 func (*UpsertRatingRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_61e79acbe641d6ef, []int{2}
+	return fileDescriptor_61e79acbe641d6ef, []int{4}
 }
 
 func (m *UpsertRatingRequest) XXX_Unmarshal(b []byte) error {
@@ -202,7 +288,7 @@ func (m *DeleteRequest) Reset()         { *m = DeleteRequest{} }
 func (m *DeleteRequest) String() string { return proto.CompactTextString(m) }
 func (*DeleteRequest) ProtoMessage()    {}
 func (*DeleteRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_61e79acbe641d6ef, []int{3}
+	return fileDescriptor_61e79acbe641d6ef, []int{5}
 }
 
 func (m *DeleteRequest) XXX_Unmarshal(b []byte) error {
@@ -240,6 +326,8 @@ func (m *DeleteRequest) GetUserId() int64 {
 func init() {
 	proto.RegisterType((*Rating)(nil), "stella.rating.v1.Rating")
 	proto.RegisterType((*GetRatingRequest)(nil), "stella.rating.v1.GetRatingRequest")
+	proto.RegisterType((*ListRatingRequests)(nil), "stella.rating.v1.ListRatingRequests")
+	proto.RegisterType((*ListRatingsResponse)(nil), "stella.rating.v1.ListRatingsResponse")
 	proto.RegisterType((*UpsertRatingRequest)(nil), "stella.rating.v1.UpsertRatingRequest")
 	proto.RegisterType((*DeleteRequest)(nil), "stella.rating.v1.DeleteRequest")
 }
@@ -247,26 +335,30 @@ func init() {
 func init() { proto.RegisterFile("ratingsvc.proto", fileDescriptor_61e79acbe641d6ef) }
 
 var fileDescriptor_61e79acbe641d6ef = []byte{
-	// 299 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xa4, 0x92, 0xc1, 0x4a, 0xc3, 0x40,
-	0x10, 0x86, 0x9b, 0x56, 0x53, 0x33, 0x58, 0x2c, 0xab, 0x68, 0x48, 0x0f, 0x86, 0x05, 0x21, 0xa7,
-	0x0d, 0xea, 0x23, 0x68, 0x91, 0xe2, 0xc9, 0x15, 0xcf, 0xd2, 0x26, 0x63, 0x08, 0x24, 0xd9, 0xb8,
-	0xbb, 0x09, 0x14, 0x5f, 0xdb, 0x07, 0x90, 0x64, 0x6d, 0x69, 0x1b, 0x2d, 0x88, 0xc7, 0x99, 0x7c,
-	0x93, 0xf9, 0xe7, 0xdf, 0x1f, 0x4e, 0xe4, 0x5c, 0xa7, 0x45, 0xa2, 0xea, 0x88, 0x95, 0x52, 0x68,
-	0x41, 0xc6, 0x4a, 0x63, 0x96, 0xcd, 0x99, 0xe9, 0xb3, 0xfa, 0xda, 0x9b, 0x24, 0x42, 0x24, 0x19,
-	0x86, 0xed, 0xf7, 0x45, 0xf5, 0x16, 0x62, 0x5e, 0xea, 0xa5, 0xc1, 0x69, 0x01, 0x36, 0x6f, 0x49,
-	0x32, 0x01, 0x07, 0x0b, 0x9d, 0xea, 0xe5, 0x6b, 0x1a, 0xbb, 0x96, 0x6f, 0x05, 0x03, 0x7e, 0x64,
-	0x1a, 0xb3, 0x98, 0x9c, 0xc1, 0xa1, 0x8a, 0x84, 0x44, 0xb7, 0xef, 0x5b, 0x41, 0x9f, 0x9b, 0x82,
-	0x5c, 0xc0, 0xb0, 0x52, 0x28, 0x9b, 0x81, 0x41, 0x3b, 0x60, 0x37, 0xe5, 0x2c, 0x26, 0x2e, 0x0c,
-	0x23, 0x91, 0xe7, 0x58, 0x68, 0xf7, 0xc0, 0xb7, 0x02, 0x87, 0xaf, 0x4a, 0x1a, 0xc2, 0xf8, 0x01,
-	0xb5, 0x59, 0xc9, 0xf1, 0xbd, 0x42, 0xa5, 0xf7, 0x6e, 0xa6, 0x1f, 0x70, 0xfa, 0x52, 0x2a, 0x94,
-	0x7f, 0x98, 0xd9, 0xd4, 0xd5, 0xdf, 0xd2, 0xb5, 0x3e, 0xc3, 0xc8, 0xfd, 0x3e, 0xe3, 0x77, 0xb5,
-	0x53, 0x18, 0xdd, 0x63, 0x86, 0x1a, 0xff, 0xb5, 0xf6, 0xe6, 0xd3, 0x82, 0x91, 0x91, 0xff, 0x8c,
-	0xb2, 0x4e, 0x23, 0x24, 0x8f, 0xe0, 0xac, 0x6d, 0x20, 0x94, 0xed, 0xbe, 0x19, 0xdb, 0xf5, 0xc8,
-	0x73, 0xbb, 0x8c, 0x01, 0x68, 0x8f, 0x3c, 0xc1, 0xf1, 0xa6, 0x45, 0xe4, 0xaa, 0xcb, 0xfe, 0x60,
-	0xe1, 0xde, 0x5f, 0xde, 0x81, 0x6d, 0x0e, 0x27, 0x97, 0x5d, 0x6a, 0xcb, 0x12, 0xef, 0x9c, 0x99,
-	0x7c, 0xb1, 0x55, 0xbe, 0xd8, 0xb4, 0xc9, 0x17, 0xed, 0x2d, 0xec, 0xb6, 0x73, 0xfb, 0x15, 0x00,
-	0x00, 0xff, 0xff, 0x1e, 0xdf, 0x99, 0xd9, 0xa4, 0x02, 0x00, 0x00,
+	// 356 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x93, 0xcf, 0x4e, 0xc2, 0x40,
+	0x10, 0xc6, 0x69, 0xd1, 0x22, 0x83, 0x44, 0x32, 0x18, 0x6d, 0xca, 0xc1, 0x66, 0xa3, 0x49, 0x4f,
+	0x4b, 0xc0, 0x47, 0x50, 0xa2, 0x44, 0x2f, 0xd6, 0x78, 0x33, 0x31, 0x50, 0x46, 0xd2, 0xa4, 0xb4,
+	0xb5, 0xbb, 0x90, 0x10, 0xdf, 0xd2, 0x27, 0x32, 0x74, 0xf9, 0x53, 0x28, 0xf4, 0xa2, 0xc7, 0x99,
+	0x7e, 0xb3, 0xdf, 0xf4, 0xf7, 0x65, 0xe0, 0x2c, 0x19, 0x48, 0x3f, 0x1c, 0x8b, 0x99, 0xc7, 0xe3,
+	0x24, 0x92, 0x11, 0x36, 0x84, 0xa4, 0x20, 0x18, 0x70, 0xd5, 0xe7, 0xb3, 0x8e, 0xd5, 0x1a, 0x47,
+	0xd1, 0x38, 0xa0, 0x76, 0xfa, 0x7d, 0x38, 0xfd, 0x6c, 0xd3, 0x24, 0x96, 0x73, 0x25, 0x67, 0x21,
+	0x18, 0x6e, 0xaa, 0xc4, 0x16, 0x54, 0x29, 0x94, 0xbe, 0x9c, 0x7f, 0xf8, 0x23, 0x53, 0xb3, 0x35,
+	0xa7, 0xec, 0x9e, 0xa8, 0x46, 0x7f, 0x84, 0xe7, 0x70, 0x2c, 0xbc, 0x28, 0x21, 0x53, 0xb7, 0x35,
+	0x47, 0x77, 0x55, 0x81, 0x97, 0x50, 0x99, 0x0a, 0x4a, 0x16, 0x03, 0xe5, 0x74, 0xc0, 0x58, 0x94,
+	0xfd, 0x11, 0x9a, 0x50, 0xf1, 0xa2, 0xc9, 0x84, 0x42, 0x69, 0x1e, 0xd9, 0x9a, 0x53, 0x75, 0x57,
+	0x25, 0x7b, 0x84, 0xc6, 0x03, 0x49, 0x65, 0xe9, 0xd2, 0xd7, 0x94, 0x84, 0x2c, 0x76, 0xce, 0x78,
+	0xe8, 0x59, 0x0f, 0xd6, 0x01, 0x7c, 0xf6, 0xc5, 0xf6, 0x53, 0xa2, 0xf0, 0x2d, 0xd6, 0x87, 0xe6,
+	0x66, 0x44, 0xb8, 0x24, 0xe2, 0x28, 0x14, 0x84, 0x5d, 0xa8, 0x2c, 0x29, 0x9a, 0x9a, 0x5d, 0x76,
+	0x6a, 0x5d, 0x93, 0xef, 0x42, 0xe4, 0x4b, 0x9b, 0x95, 0x90, 0x7d, 0x43, 0xf3, 0x2d, 0x16, 0x94,
+	0xfc, 0xc7, 0xaf, 0x6c, 0xe8, 0x2a, 0x8a, 0x4b, 0xba, 0x87, 0x21, 0xf6, 0xa0, 0x7e, 0x4f, 0x01,
+	0x49, 0xfa, 0x93, 0x6d, 0xf7, 0x47, 0x87, 0xba, 0x5a, 0xff, 0x95, 0x92, 0x99, 0xef, 0x11, 0x3e,
+	0x41, 0x75, 0x9d, 0x0e, 0xb2, 0x3c, 0x85, 0xdd, 0xe8, 0xac, 0x83, 0xa4, 0x58, 0x09, 0xdf, 0xa1,
+	0x96, 0xa1, 0x8d, 0xd7, 0x79, 0x69, 0x3e, 0x3f, 0xeb, 0xa6, 0x48, 0xb5, 0x8e, 0x8c, 0x95, 0xf0,
+	0x05, 0x4e, 0xb3, 0x01, 0xe0, 0x9e, 0xc1, 0x3d, 0x01, 0x15, 0x2e, 0x7c, 0x07, 0x86, 0xc2, 0x8a,
+	0x57, 0x79, 0xd5, 0x16, 0x70, 0xeb, 0x82, 0xab, 0xa3, 0xe2, 0xab, 0xa3, 0xe2, 0xbd, 0xc5, 0x51,
+	0xb1, 0xd2, 0xd0, 0x48, 0x3b, 0xb7, 0xbf, 0x01, 0x00, 0x00, 0xff, 0xff, 0x62, 0xfc, 0x8f, 0x1c,
+	0x99, 0x03, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -282,6 +374,7 @@ const _ = grpc.SupportPackageIsVersion4
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type RatingServiceClient interface {
 	GetRating(ctx context.Context, in *GetRatingRequest, opts ...grpc.CallOption) (*Rating, error)
+	ListRatings(ctx context.Context, in *ListRatingRequests, opts ...grpc.CallOption) (*ListRatingsResponse, error)
 	UpsertRating(ctx context.Context, in *UpsertRatingRequest, opts ...grpc.CallOption) (*Rating, error)
 	Delete(ctx context.Context, in *DeleteRequest, opts ...grpc.CallOption) (*empty.Empty, error)
 }
@@ -297,6 +390,15 @@ func NewRatingServiceClient(cc *grpc.ClientConn) RatingServiceClient {
 func (c *ratingServiceClient) GetRating(ctx context.Context, in *GetRatingRequest, opts ...grpc.CallOption) (*Rating, error) {
 	out := new(Rating)
 	err := c.cc.Invoke(ctx, "/stella.rating.v1.RatingService/GetRating", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *ratingServiceClient) ListRatings(ctx context.Context, in *ListRatingRequests, opts ...grpc.CallOption) (*ListRatingsResponse, error) {
+	out := new(ListRatingsResponse)
+	err := c.cc.Invoke(ctx, "/stella.rating.v1.RatingService/ListRatings", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -324,6 +426,7 @@ func (c *ratingServiceClient) Delete(ctx context.Context, in *DeleteRequest, opt
 // RatingServiceServer is the server API for RatingService service.
 type RatingServiceServer interface {
 	GetRating(context.Context, *GetRatingRequest) (*Rating, error)
+	ListRatings(context.Context, *ListRatingRequests) (*ListRatingsResponse, error)
 	UpsertRating(context.Context, *UpsertRatingRequest) (*Rating, error)
 	Delete(context.Context, *DeleteRequest) (*empty.Empty, error)
 }
@@ -334,6 +437,9 @@ type UnimplementedRatingServiceServer struct {
 
 func (*UnimplementedRatingServiceServer) GetRating(ctx context.Context, req *GetRatingRequest) (*Rating, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetRating not implemented")
+}
+func (*UnimplementedRatingServiceServer) ListRatings(ctx context.Context, req *ListRatingRequests) (*ListRatingsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListRatings not implemented")
 }
 func (*UnimplementedRatingServiceServer) UpsertRating(ctx context.Context, req *UpsertRatingRequest) (*Rating, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UpsertRating not implemented")
@@ -360,6 +466,24 @@ func _RatingService_GetRating_Handler(srv interface{}, ctx context.Context, dec 
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(RatingServiceServer).GetRating(ctx, req.(*GetRatingRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _RatingService_ListRatings_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ListRatingRequests)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(RatingServiceServer).ListRatings(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/stella.rating.v1.RatingService/ListRatings",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(RatingServiceServer).ListRatings(ctx, req.(*ListRatingRequests))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -407,6 +531,10 @@ var _RatingService_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetRating",
 			Handler:    _RatingService_GetRating_Handler,
+		},
+		{
+			MethodName: "ListRatings",
+			Handler:    _RatingService_ListRatings_Handler,
 		},
 		{
 			MethodName: "UpsertRating",
