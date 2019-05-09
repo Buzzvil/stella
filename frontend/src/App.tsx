9e79de0f9b6e0b36ec42f407cb5b435feafe9c09
Benjamin Baldivia <goblinlordx@gmail.com>
2019-05-09 15:20:26 +0900

    Comment as-is

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-// import { BookServiceClient } from './proto/BooksvcServiceClientPb';
-// import { ListBooksRequest, ListBooksResponse, Book } from './proto/booksvc_pb';
+// import { BookServiceClient } from 'proto/booksvc_grpc_web_pb';
+// import { ListBooksRequest, ListBooksResponse, Book } from 'proto/booksvc_pb';
 import AppHeader from './AppHeader';
 import './App.css';
 
 const theme = createMuiTheme({});
 
-// const bookService = new BookServiceClient('http://localhost:30080', null, null);
+// const bookService = new BookServiceClient('https://billy.buzzvil.com', null, null);
 
 function App() {
-  const [query, setQuery] = useState("");
+  // const [query, setQuery] = useState("");
   // const [books, setBooks] = useState<Book[] | null>(null);
 
   // const listBooks = ((query: string) => {
   //   const req = new ListBooksRequest();
-  //   req.setName(query)
-  //   bookService.listBooks(req, {}, (err: any, resp: ListBooksResponse) => {
+  //   req.setFilter(query)
+  //   const meta = {'authorization': 'xxxxx'}
+  //   bookService.listBooks(req, meta, (err: any, resp: ListBooksResponse) => {
   //     if (err) {
   //       console.log(err);
   //     } else {
+  //       console.log(resp.getBooksList());
   //       setBooks(resp.getBooksList());
   //     }
   //   });
@@ -33,7 +35,7 @@ function App() {
     <MuiThemeProvider theme={theme}>
       <div className="App">
         <AppHeader />
-        <input value={query} onChange={e => setQuery(e.target.value)} type="search" />
+        {/* <input value={query} onChange={e => setQuery(e.target.value)} type="search" /> */}
         {/* {books && books.map((book) => <p key={book.getId()}>{book.getName()}</p>)} */}
       </div>
     </MuiThemeProvider>
