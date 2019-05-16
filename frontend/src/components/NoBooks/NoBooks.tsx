3cbf4f8b72411982e85e375b44c509b90b5724a9
phil.lee <phil.lee@buzzvil.com>
2019-05-16 13:51:11 +0900

    card width

--- frontend/src/components/NoBooks/NoBooks.tsx
+++ frontend/src/components/NoBooks/NoBooks.tsx
@@ -1,14 +1,21 @@
 import React, { ReactComponentElement } from "react";
-import Card from '@material-ui/core/Card';
 import RoundButton from "../RoundButton/RoundButton";
 import styled from "styled-components";
-import { ReactComponent as SadIcon} from "../../img/emoticon_sad.svg";
+import { ReactComponent as SadIcon} from "../../img/ic_emoticon_sad.svg";
 
-const Container = styled.div`
+const Card = styled.div`
+    width: 472px;
+    background: #ffffff;
+    padding-top: 35px;
+    padding-bottom: 24px;
     text-align: center;
+    border-radius: 16px;
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.2), 0px 2px 2px rgba(0, 0, 0, 0.12), 0px 0px 2px rgba(0, 0, 0, 0.14);
 `;
 
 const Title = styled.h3`
+    margin-top: 22px;
+    margin-bottom: 0px;
     font-size: 34px;
     font-weight: 500;
     line-height: 49px;
@@ -16,6 +23,8 @@ const Title = styled.h3`
 `;
 
 const Desc = styled.p`
+    margin-top: 16px;
+    margin-bottom: 24px;
     font-size: 14px;
     line-height: 16px;
     text-align: center;
@@ -26,12 +35,10 @@ const Desc = styled.p`
 export default () => {
     return (
         <Card>
-            <Container>
-                    <SadIcon />
-                    <Title>없어요...</Title>
-                    <Desc>It’s not there yet, feel free to request it!</Desc>
-                    <RoundButton>REQUEST A BOOK</RoundButton>
-            </Container>
+            <SadIcon />
+            <Title>없어요...</Title>
+            <Desc>It’s not there yet, feel free to request it!</Desc>
+            <RoundButton>REQUEST A BOOK</RoundButton>
         </Card>
     );
 }
