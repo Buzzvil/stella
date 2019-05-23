8185f68f7ee1f947b1a616e6a1555d1ef24ecf9d
Seohyun Kwak <summer.kwak@buzzvil.com>
2019-05-23 13:07:01 +0900

    comment

--- booksvc/internal/app/crawlersrv/runner.go
+++ booksvc/internal/app/crawlersrv/runner.go
@@ -8,16 +8,19 @@ import (
 	"github.com/Buzzvil/stella/booksvc/internal/pkg/crawler/crawlerrepo"
 )
 
+// Runner is interface for crawler
 type Runner struct {
 	u crawler.Usecase
 }
 
+// NewRunner initializes runner
 func NewRunner(key string) Runner {
 	r := crawlerrepo.New(key)
 	u := crawler.NewUsecase(r)
 	return Runner{u: u}
 }
 
+// Start search book with query
 func (r *Runner) Start(query string) error {
 	book, err := r.u.Search(query)
 	if err != nil {
