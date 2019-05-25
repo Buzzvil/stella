f6d73911056f080868124f532731c4118acc6a5d
Hoseong Hwang <liam.hwang@buzzvil.com>
2019-05-25 12:05:42 +0900

    Remove git user setting

--- .github/actions/deploy-storybook/entrypoint.sh
+++ .github/actions/deploy-storybook/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-git config --global user.name "${GITHUB_ACTOR}"
-git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-remote_repo="git@github.com:${GITHUB_REPOSITORY}.git"
-git remote add origin "${remote_repo}"
-
 cd frontend
 echo 'Installing npm'
 npm install
