e1f98b87f9628ccfbdf9ba94f40b4858d27a251e
Seohyun Kwak <summer.kwak@buzzvil.com>
2019-05-30 13:09:29 +0900

    search and create with csv input

--- booksvc/cmd/bookcrawler/main.go
+++ booksvc/cmd/bookcrawler/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
 	"fmt"
 	"os"
 	"strings"
@@ -9,13 +11,16 @@ import (
 )
 
 func main() {
+	file, _ := os.Open("./book.csv")
+	rdr := csv.NewReader(bufio.NewReader(file))
+	rows, _ := rdr.ReadAll()
 	r := crawlersrv.NewRunner(os.Getenv("KAKAO_API_KEY"))
-	title := ""
-	author := ""
-	publisher := ""
-	query := strings.Join([]string{title, author, publisher}, " ")
-	err := r.Start(query)
-	if err != nil {
-		fmt.Println(err)
+	for _, row := range rows {
+		pubs := strings.Fields(row[1])
+		query := strings.Join([]string{row[0], pubs[0], row[2]}, " ")
+		err := r.Start(query)
+		if err != nil {
+			fmt.Printf("%s: %s \n", err, query)
+		}
 	}
 }
