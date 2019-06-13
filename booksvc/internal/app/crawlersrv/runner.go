51b5abc7eaad8cb57ded96f97a4f21369439f4a6
Seohyun Kwak <summer.kwak@buzzvil.com>
2019-06-13 13:02:02 +0900

    Use Usecase to create book at runner

--- booksvc/internal/app/crawlersrv/runner.go
+++ booksvc/internal/app/crawlersrv/runner.go
@@ -1,35 +1,53 @@
 package crawlersrv
 
 import (
+	"database/sql"
 	"errors"
 	"fmt"
 
+	"github.com/Buzzvil/stella/booksvc/internal/pkg/book"
+	"github.com/Buzzvil/stella/booksvc/internal/pkg/book/bookrepo"
 	"github.com/Buzzvil/stella/booksvc/internal/pkg/crawler"
 	"github.com/Buzzvil/stella/booksvc/internal/pkg/crawler/crawlerrepo"
 )
 
 // Runner is interface for crawler
 type Runner struct {
-	u crawler.Usecase
+	u  crawler.Usecase
+	bu book.Usecase
 }
 
 // NewRunner initializes runner
-func NewRunner(key string) Runner {
+func NewRunner(key string, db *sql.DB) Runner {
 	r := crawlerrepo.New(key)
 	u := crawler.NewUsecase(r)
-	return Runner{u: u}
+	br := bookrepo.New(db)
+	bu := book.NewUsecase(br)
+	return Runner{u: u, bu: bu}
 }
 
 // Start search book with query
 func (r *Runner) Start(query string) error {
-	book, err := r.u.Search(query)
+	b, err := r.u.Search(query)
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
-	if book == nil {
+	if b == nil {
 		return errors.New("Not found")
 	}
-	fmt.Println("Book Found: " + book.Name)
+
+	book := book.Book{
+		Name:      b.Name,
+		Isbn:      b.Isbn,
+		Authors:   b.Authors,
+		Publisher: b.Publisher,
+		Content:   b.Content,
+	}
+
+	_, e := r.bu.CreateBook(book)
+	if e != nil {
+		return e
+	}
 	return nil
 }
