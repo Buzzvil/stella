aa606607ab661c8e3e53a464e356451ec820c220
Ben Pyungwoo Yoo <hovan@hovans.com>
2019-07-04 17:38:02 +0900

    Add not_found component (#61)

--- frontend/src/components/NoBooks/NoBooks.stories.tsx
+++ frontend/src/components/NoBooks/NoBooks.stories.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import NoBooks from "./NoBooks";
+import styled from "styled-components"
 
 import { MuiThemeProvider } from "@material-ui/core";
 
 import StandardTheme from "../../theme/StandardTheme";
 
+const Frame = styled.div`
+    margin-top: 91px;
+`;
+
 storiesOf("Components|No Books", module)
   .add("Oops", () => (
     <MuiThemeProvider theme={StandardTheme}>
+      <Frame>
         <NoBooks />
+      </Frame>
     </MuiThemeProvider>
   ));
