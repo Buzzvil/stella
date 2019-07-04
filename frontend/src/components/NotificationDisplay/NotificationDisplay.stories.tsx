8d878d67ce23f1cef27e8b360794624b29a68a9d
Benjamin Baldivia <goblinlordx@gmail.com>
2019-07-04 17:11:18 +0900

    Separate usage of msg adder and display for example

--- frontend/src/components/NotificationDisplay/NotificationDisplay.stories.tsx
+++ frontend/src/components/NotificationDisplay/NotificationDisplay.stories.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 
 import { storiesOf } from "@storybook/react";
 import NotificationDisplay from "./NotificationDisplay";
-import { NotificationProvider } from "../../hooks/NotificationContext/NotificationContext";
+import NotificationContext, {
+  NotificationProvider,
+  NewMsg
+} from "../../hooks/NotificationContext/NotificationContext";
 
+const AddButton: React.SFC = () => {
+  const [n, setN] = useState(0);
+  const [_, dispatch] = useContext(NotificationContext);
+  return (
+    <button
+      onClick={() => {
+        setN(n + 1);
+        NewMsg(dispatch, { msg: `Test Message ${n}` });
+      }}
+    >
+      Add New Message
+    </button>
+  );
+};
 
-storiesOf("Organisms|NotificationDisplay", module).add("Default", () => (
+storiesOf("Organisms|NotificationDisplay", module).add("Default", () => {
+  return (
     <NotificationProvider>
-        <NotificationDisplay></NotificationDisplay>
+      <AddButton />
+      <NotificationDisplay />
     </NotificationProvider>
-));
+  );
+});
--- frontend/src/components/NotificationDisplay/NotificationDisplay.tsx
+++ frontend/src/components/NotificationDisplay/NotificationDisplay.tsx
@@ -1,35 +1,20 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import NotificationContext, {
-  NewMsg,
   NextMsg
 } from "../../hooks/NotificationContext/NotificationContext";
 
 const delay = (t: number) => new Promise(resolve => setTimeout(resolve, t));
 
 export default () => {
-  const [n, setN] = useState(0);
   const [state, dispatch] = useContext(NotificationContext);
 
   useEffect(() => {
-    if (!state[0]) return;
+    if (!state.length) return;
     delay(1000).then(() => NextMsg(dispatch));
   }, [state[0]]);
 
   return (
     <div>
-      <button
-        onClick={() => {
-          setN(n + 1);
-          dispatch({
-            type: "QUEUE",
-            data: {
-              msg: `Test Message ${n}`
-            }
-          });
-        }}
-      >
-        Add New Message
-      </button>
       {state.length ? (
         <div>
           <h1>Displaying:</h1>
