3c6c79fdea3925469dbaedd575ca812033abeaac
phil.lee <phil.lee@buzzvil.com>
2019-07-08 20:52:09 +0900

    wrapping memory router

--- frontend/src/components/SignInPage/SignInPage.test.tsx
+++ frontend/src/components/SignInPage/SignInPage.test.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { MemoryRouter } from 'react-router-dom';
 
 import SignInPage from "./SignInPage";
 
 it("renders at least one button", () => {
-  const instance = renderer.create(<SignInPage />).root;
+  const instance = renderer.create(
+    <MemoryRouter>
+      <SignInPage />
+    </MemoryRouter>
+  ).root;
   const buttons = instance.findAllByType("button");
   expect(buttons.length).toBeGreaterThan(0);
 });
