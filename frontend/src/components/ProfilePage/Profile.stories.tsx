6592ba4f1e1280e12679ba0aa2e025cb1894550f
Seohyun Kwak <summer.kwak@buzzvil.com>
2019-07-25 12:39:35 +0900

    Add profile page

---
+++ frontend/src/components/ProfilePage/Profile.stories.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
+
+import StandardTheme from "../../theme/StandardTheme";
+import ProfilePage from "./ProfilePage";
+import { User } from "proto/usersvc_pb";
+import { Book } from "proto/booksvc_pb";
+
+const currentUser = (() => {
+    const user = new User();
+    user.setId(1);
+    user.setName("John Doe")
+    user.setImage("https://placekitten.com/g/200/300")
+    return user;
+})();
+
+const TestBook = (() => {
+    const book = new Book();
+    book.setId(8331);
+    book.setName("Test Book");
+    book.setAuthorsList(["Me", "Myself", "I"]);
+    book.setCoverImage("https://placekitten.com/g/200/300");
+    return book;
+})();
+
+storiesOf("Pages|ProfilePage", module)
+    .add("default", () => (
+        <MuiThemeProvider theme={StandardTheme}>
+            <CssBaseline />
+            <ProfilePage currentUser={currentUser} readingBooks={[TestBook, TestBook, TestBook]} />
+        </MuiThemeProvider>
+    ));
---
+++ frontend/src/components/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Typography, Avatar, TextField, Grid } from "@material-ui/core";
+import { AvatarProps } from '@material-ui/core/Avatar';
+import { Book } from "proto/booksvc_pb";
+import { User } from "proto/usersvc_pb";
+import { ResourceStatus } from "proto/rentalsvc_pb";
+import { makeStyles, createStyles } from '@material-ui/styles';
+
+const profileStyle = makeStyles(
+    createStyles({
+        avatar: {
+            width: 100,
+            height: 100,
+        },
+        lastItem: {
+            display: 'none'
+        }
+    })
+)
+
+const ProfileContainer = styled.div`
+  display: flex;
+  min-height: 652px;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ProfileContent = styled.div`
+ width: 100%;
+  max-width: 720px;
+`;
+
+const NowReading = styled.div`
+  font-wiehgt: bold;
+  font-size: 36px;
+  line-height: 50px;
+  color: white;
+`;
+
+const ProfileHeader = styled.div`
+  width: 100%;
+  max-width: 720px;
+`;
+
+interface ProfilePageProps {
+    currentUser?: User
+    readingBooks: Book[]
+}
+
+const ProfilePage: React.SFC<ProfilePageProps & AvatarProps> = ({
+    currentUser,
+    readingBooks
+}) => {
+    // const [haveSearched, setSearched] = useState(false);
+    // const [query, setQuery] = useState('');
+    const classes = profileStyle();
+
+    return (
+        <ProfileContainer>
+            <ProfileHeader>
+                {currentUser &&
+                    <Avatar alt={currentUser.getName()} src={currentUser.getImage()} className={classes.avatar} />
+                }
+            </ProfileHeader>
+            <ProfileContent>
+                {currentUser &&
+                    <NowReading>
+                        {currentUser.getName()} is reading&nbsp;
+                        {readingBooks.map((book, key) =>
+                            <span>
+                                <span>{book.getName()}</span>
+                                <span>{key == readingBooks.length - 1 ? "." : (key == readingBooks.length - 2 ? " and " : ", ")}</span>
+                            </span>
+                        )}
+                    </NowReading>
+                }
+            </ProfileContent>
+        </ProfileContainer>
+    );
+};
+
+export default ProfilePage;
