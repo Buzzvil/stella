da01a8b159dd790b8c416a483db07a3dda7e2e29
Seohyun Kwak <summer.kwak@buzzvil.com>
2019-07-30 14:37:11 +0900

    clean up

--- frontend/src/components/ProfilePage/Profile.stories.tsx
+++ frontend/src/components/ProfilePage/Profile.stories.tsx
@@ -6,8 +6,7 @@ import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
 import StandardTheme from "../../theme/StandardTheme";
 import ProfilePage from "./ProfilePage";
 import { User } from "proto/usersvc_pb";
-import { Book } from "proto/booksvc_pb";
-import { MemoryRouter, withRouter } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 import { createMemoryHistory, createLocation } from 'history';
 import { match } from 'react-router';
 
@@ -17,15 +16,6 @@ const currentUser = (() => {
     user.setName("John Doe")
     user.setImage("https://placekitten.com/g/200/300")
     return user;
-})();
-
-const TestBook = (() => {
-    const book = new Book();
-    book.setId(8331);
-    book.setName("Test Book");
-    book.setAuthorsList(["Me", "Myself", "I"]);
-    book.setCoverImage("https://placekitten.com/g/200/300");
-    return book;
 })();
 
 const history = createMemoryHistory();
